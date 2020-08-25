f85400ea4b0a90cc8b91ef6701ea476819864128
Hyeon Kim <simnalamburt@gmail.com>
2020-08-26 03:37:05 +0900

    New implementation: emotion-styled

---
+++ src/implementations/emotion-styled/Dot.js
@@ -0,0 +1,21 @@
+import styled from '@emotion/styled';
+import View from './View';
+
+const Dot = styled(View)`
+  position: absolute;
+  cursor: pointer;
+  width: 0;
+  height: 0;
+  margin-left: ${props => props.x}px;
+  margin-top: ${props => props.y}px;
+  transform: translate(50%, 50%);
+
+  border-style: solid;
+  border-top-width: 0;
+  border-bottom-width: ${props => props.size / 2}px;
+  border-right-width: ${props => props.size / 2}px;
+  border-left-width: ${props => props.size / 2}px;
+  border-bottom-color: ${props => props.color};
+`;
+
+export default Dot;
