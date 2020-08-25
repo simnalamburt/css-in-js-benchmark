1939472a2c09020fffedf362c27c8a73e07b71ba
Hyeon Kim <simnalamburt@gmail.com>
2020-08-26 03:19:56 +0900

    SierpinskiTriangle.js: Simplify codes

--- src/cases/SierpinskiTriangle.js
+++ src/cases/SierpinskiTriangle.js
@@ -9,59 +9,59 @@ export default function SierpinskiTriangle(props) {
   let { s } = props;
   const { Dot } = components;
 
-  if (Dot) {
-    if (s <= targetSize) {
-      let fn;
-      switch (depth) {
-        case 1:
-          fn = interpolatePurples;
-          break;
-        case 2:
-          fn = interpolateBuPu;
-          break;
-        case 3:
-        default:
-          fn = interpolateRdPu;
-      }
-
-      // introduce randomness to ensure that repeated runs don't produce the same colors
-      const color = fn((renderCount * Math.random()) / 20);
-      return <Dot color={color} size={targetSize} x={x - targetSize / 2} y={y - targetSize / 2} />;
-    }
-
-    s /= 2;
-
-    return (
-      <React.Fragment>
-        <SierpinskiTriangle
-          components={components}
-          depth={1}
-          renderCount={renderCount}
-          s={s}
-          x={x}
-          y={y - s / 2}
-        />
-        <SierpinskiTriangle
-          components={components}
-          depth={2}
-          renderCount={renderCount}
-          s={s}
-          x={x - s}
-          y={y + s / 2}
-        />
-        <SierpinskiTriangle
-          components={components}
-          depth={3}
-          renderCount={renderCount}
-          s={s}
-          x={x + s}
-          y={y + s / 2}
-        />
-      </React.Fragment>
-    );
-  } else {
+  if (Dot == null) {
     return <span style={{ color: 'white' }}>No implementation available</span>;
   }
+
+  if (s <= targetSize) {
+    let fn;
+    switch (depth) {
+      case 1:
+        fn = interpolatePurples;
+        break;
+      case 2:
+        fn = interpolateBuPu;
+        break;
+      case 3:
+      default:
+        fn = interpolateRdPu;
+    }
+
+    // introduce randomness to ensure that repeated runs don't produce the same colors
+    const color = fn((renderCount * Math.random()) / 20);
+    return <Dot color={color} size={targetSize} x={x - targetSize / 2} y={y - targetSize / 2} />;
+  }
+
+  s /= 2;
+
+  return (
+    <>
+      <SierpinskiTriangle
+        components={components}
+        depth={1}
+        renderCount={renderCount}
+        s={s}
+        x={x}
+        y={y - s / 2}
+      />
+      <SierpinskiTriangle
+        components={components}
+        depth={2}
+        renderCount={renderCount}
+        s={s}
+        x={x - s}
+        y={y + s / 2}
+      />
+      <SierpinskiTriangle
+        components={components}
+        depth={3}
+        renderCount={renderCount}
+        s={s}
+        x={x + s}
+        y={y + s / 2}
+      />
+    </>
+  );
 }
 
 SierpinskiTriangle.benchmarkType = BenchmarkType.UPDATE;
