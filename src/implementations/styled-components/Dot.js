a934e06bccb629292467a55dce062d9eeba5f785
Hyeon Kim <simnalamburt@gmail.com>
2020-08-26 02:51:42 +0900

    styled-components: Fix Dot.js

--- src/implementations/styled-components/Dot.js
+++ src/implementations/styled-components/Dot.js
@@ -1,21 +1,36 @@
 import styled from 'styled-components';
 import View from './View';
 
-const Dot = styled(View)`
+// Styled components yields warning like below if users creates too many
+// classes. Thus, I'll consider this code as realistic scenario.
+//
+//   main.js:32812 Over 200 classes were generated for component View with the id of "View-sc-9o5veo-0".
+//   Consider using the attrs method, together with a style object for frequently changed styles.
+//   Example:
+//     const Component = styled.div.attrs(props => ({
+//       style: {
+//         background: props.background,
+//       },
+//     }))`width: 100%;`
+//
+//     <Component />
+const Dot = styled(View).attrs(props => ({
+  style: {
+    marginLeft: `${props.x}px`,
+    marginTop: `${props.y}px`,
+    borderRightWidth: `${props.size / 2}px`,
+    borderBottomWidth: `${props.size / 2}px`,
+    borderLeftWidth: `${props.size / 2}px`,
+    borderBottomColor: `${props.color}`
+  }
+}))`
   position: absolute;
   cursor: pointer;
   width: 0;
   height: 0;
-  margin-left: ${props => props.x}px;
-  margin-top: ${props => props.y}px;
   transform: translate(50%, 50%);
-
   border-style: solid;
   border-top-width: 0;
-  border-bottom-width: ${props => props.size / 2}px;
-  border-right-width: ${props => props.size / 2}px;
-  border-left-width: ${props => props.size / 2}px;
-  border-bottom-color: ${props => props.color};
 `;
 
 export default Dot;
