1455aca238b002342bca0b8e8015be14b8c04c39
Hyeon Kim <simnalamburt@gmail.com>
2020-08-26 05:56:49 +0900

    emotion-vanilla*: Make benchmark more realistic

--- src/implementations/emotion-vanilla-v11/View.js
+++ src/implementations/emotion-vanilla-v11/View.js
@@ -1,24 +1,27 @@
 import { css } from '@emotion-v11/css';
 import React from 'react';
 
-const viewStyle = css`
-  align-items: stretch;
-  border-width: 0;
-  border-style: solid;
-  box-sizing: border-box;
-  display: flex;
-  flex-basis: auto;
-  flex-direction: column;
-  flex-shrink: 0;
-  margin: 0;
-  padding: 0;
-  position: relative;
-  /* fix flexbox bugs */
-  min-height: 0;
-  min-width: 0;
-`;
-
 export default function View(props) {
   const { className = '', ...other } = props;
-  return <div {...other} className={`${viewStyle} ${className}`} />;
+  return (
+    <div
+      {...other}
+      className={`${css`
+        align-items: stretch;
+        border-width: 0;
+        border-style: solid;
+        box-sizing: border-box;
+        display: flex;
+        flex-basis: auto;
+        flex-direction: column;
+        flex-shrink: 0;
+        margin: 0;
+        padding: 0;
+        position: relative;
+        /* fix flexbox bugs */
+        min-height: 0;
+        min-width: 0;
+      `} ${className}`}
+    />
+  );
 }
--- src/implementations/emotion-vanilla/Box.js
+++ src/implementations/emotion-vanilla/Box.js
@@ -5,42 +5,37 @@ import { css } from 'emotion';
 const Box = ({ color, fixed = false, layout = 'column', outer = false, ...other }) => (
   <View
     {...other}
-    className={`${styles[`color${color}`] ?? ''} ${fixed ? styles.fixed : ''} ${
-      layout === 'row' ? styles.row : ''
-    } ${outer ? styles.outer : ''}`}
+    className={css`
+      align-self: flex-start;
+      flex-direction: ${layout};
+      padding: ${outer ? 4 : 0}px;
+      ${fixed &&
+      `
+        width: 6px;
+        height: 6px;
+      `}
+      background-color: ${getColor(color)};
+    `}
   />
 );
 
-const styles = {
-  outer: css`
-    align-self: flex-start;
-    padding: 4px;
-  `,
-  row: css`
-    flex-direction: row;
-  `,
-  color0: css`
-    background-color: #14171a;
-  `,
-  color1: css`
-    background-color: #aab8c2;
-  `,
-  color2: css`
-    background-color: #e6ecf0;
-  `,
-  color3: css`
-    background-color: #ffad1f;
-  `,
-  color4: css`
-    background-color: #f45d22;
-  `,
-  color5: css`
-    background-color: #e0245e;
-  `,
-  fixed: css`
-    width: 6px;
-    height: 6px;
-  `
-};
+function getColor(color) {
+  switch (color) {
+    case 0:
+      return '#14171A';
+    case 1:
+      return '#AAB8C2';
+    case 2:
+      return '#E6ECF0';
+    case 3:
+      return '#FFAD1F';
+    case 4:
+      return '#F45D22';
+    case 5:
+      return '#E0245E';
+    default:
+      return 'transparent';
+  }
+}
 
 export default Box;
