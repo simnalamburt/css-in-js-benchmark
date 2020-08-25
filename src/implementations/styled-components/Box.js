41db87ea7cc8bd50b57d6a39ef76b55bafc17b61
Hyeon Kim <simnalamburt@gmail.com>
2020-08-26 01:52:19 +0900

    styled-components: More practical bench

--- src/implementations/styled-components/Box.js
+++ src/implementations/styled-components/Box.js
@@ -24,8 +24,9 @@ const Box = styled(View)`
   align-self: flex-start;
   flex-direction: ${props => (props.layout === 'column' ? 'column' : 'row')};
   padding: ${props => (props.outer ? '4px' : '0')};
-  ${props => props.fixed && 'height:6px;'} ${props =>
-    props.fixed && 'width:6px;'} background-color: ${props => getColor(props.color)};
+  ${props => props.fixed && 'height: 6px;'}
+  ${props => props.fixed && 'width: 6px;'}
+  background-color: ${props => getColor(props.color)};
 `;
 
 export default Box;
--- src/implementations/styled-components/Dot.js
+++ src/implementations/styled-components/Dot.js
@@ -1,24 +1,21 @@
 import styled from 'styled-components';
 import View from './View';
 
-const Dot = styled(View).attrs({
-  style: props => ({
-    marginLeft: `${props.x}px`,
-    marginTop: `${props.y}px`,
-    borderRightWidth: `${props.size / 2}px`,
-    borderBottomWidth: `${props.size / 2}px`,
-    borderLeftWidth: `${props.size / 2}px`,
-    borderBottomColor: `${props.color}`
-  })
-})`
+const Dot = styled(View)`
   position: absolute;
   cursor: pointer;
   width: 0;
   height: 0;
-  border-color: transparent;
+  margin-left: ${props => props.x}px;
+  margin-top: ${props => props.y}px;
+  transform: translate(50%, 50%);
+
   border-style: solid;
   border-top-width: 0;
-  transform: translate(50%, 50%);
+  border-bottom-width: ${props => props.size / 2}px;
+  border-right-width: ${props => props.size / 2}px;
+  border-left-width: ${props => props.size / 2}px;
+  border-bottom-color: ${props => props.color};
 `;
 
 export default Dot;
