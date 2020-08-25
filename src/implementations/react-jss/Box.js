075f852f2fff3351f3b4abdcab85c9c38adc6dac
Hyeon Kim <simnalamburt@gmail.com>
2020-08-25 21:36:15 +0900

    Use createUseStyles instaed of injectSheet

--- src/implementations/react-jss/Box.js
+++ src/implementations/react-jss/Box.js
@@ -1,50 +1,48 @@
-import classnames from 'classnames';
-import injectSheet from 'react-jss';
+import { createUseStyles } from 'react-jss';
 import React from 'react';
 import View from './View';
 
-const Box = ({ classes, color, fixed = false, layout = 'column', outer = false, ...other }) => (
-  <View
-    {...other}
-    className={classnames({
-      [classes[`color${color}`]]: true,
-      [classes.fixed]: fixed,
-      [classes.row]: layout === 'row',
-      [classes.outer]: outer
-    })}
-  />
-);
+const useStyles = createUseStyles(theme => ({
+  root: (props) => ({
+    ...(props.outer && {
+      alignSelf: 'flex-start',
+      padding: 4
+    }),
+    ...(props.layout === 'row' && {
+      flexDirection: 'row'
+    }),
+    ...(props.color === 0 && {
+      backgroundColor: '#14171A'
+    }),
+    ...(props.color === 1 && {
+      backgroundColor: '#AAB8C2'
+    }),
+    ...(props.color === 2 && {
+      backgroundColor: '#E6ECF0'
+    }),
+    ...(props.color === 3 && {
+      backgroundColor: '#FFAD1F'
+    }),
+    ...(props.color === 4 && {
+      backgroundColor: '#F45D22'
+    }),
+    ...(props.color === 5 && {
+      backgroundColor: '#E0245E'
+    }),
+    ...(props.fixed && {
+      width: 6,
+      height: 6
+    }),
+  })
+}));
 
-const styles = {
-  outer: {
-    alignSelf: 'flex-start',
-    padding: 4
-  },
-  row: {
-    flexDirection: 'row'
-  },
-  color0: {
-    backgroundColor: '#14171A'
-  },
-  color1: {
-    backgroundColor: '#AAB8C2'
-  },
-  color2: {
-    backgroundColor: '#E6ECF0'
-  },
-  color3: {
-    backgroundColor: '#FFAD1F'
-  },
-  color4: {
-    backgroundColor: '#F45D22'
-  },
-  color5: {
-    backgroundColor: '#E0245E'
-  },
-  fixed: {
-    width: 6,
-    height: 6
-  }
+const Box = ({ color, fixed = false, layout = 'column', outer = false, ...other }) => {
+  const classes = useStyles({ color, fixed, layout, outer, ...other });
+
+  return <View
+    {...other}
+    className={classes.root}
+  />
 };
 
-export default injectSheet(styles)(Box);
+export default Box;
