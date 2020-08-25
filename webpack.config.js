0fdcdda36c1a341c633028988e344088535ca972
Hyeon Kim <simnalamburt@gmail.com>
2020-08-26 01:08:34 +0900

    Use webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,10 @@ module.exports = {
       }
     ]
   },
+  devtool: 'source-map',
+  devServer: {
+    publicPath: '/dist/'
+  },
   performance: {
     // This benchmark uses lots of libraries
     maxEntrypointSize: 800000,
