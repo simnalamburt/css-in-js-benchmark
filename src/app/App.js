7c234674cd9616d73ae51bc4e9a2948827989b8e
Hyeon Kim <simnalamburt@gmail.com>
2020-08-25 22:24:59 +0900

    Select styled-components by default

--- src/app/App.js
+++ src/app/App.js
@@ -18,7 +18,7 @@ export default class App extends Component {
     const currentBenchmarkName = Object.keys(props.tests)[0];
     this.state = {
       currentBenchmarkName,
-      currentLibraryName: 'react-native-web',
+      currentLibraryName: 'styled-components',
       status: 'idle',
       results: []
     };
