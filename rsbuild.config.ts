59e30133e2c01efe81ffe2b40633f77c724814a0
Jihyeon Kim <simnalamburt@gmail.com>
2025-03-14 18:33:41 +0900

    rsbuild.config.ts: Use relative assetPrefix

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -2,5 +2,8 @@ import { defineConfig } from '@rsbuild/core';
 import { pluginReact } from '@rsbuild/plugin-react';
 
 export default defineConfig({
-  plugins: [pluginReact()]
+  plugins: [pluginReact()],
+  output: {
+    assetPrefix: './'
+  }
 });
